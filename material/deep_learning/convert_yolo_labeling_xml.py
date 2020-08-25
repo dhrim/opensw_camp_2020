beef2698e8d561910bee9741d05e092e36d0f132
dohyoung rim <dh-rim@hanmail.net>
2020-08-25 16:44:18 +0900

    Update convert_yolo_labeling_xml.py
    yolo labeling 데이터 컨버전 버그 픽스

--- material/deep_learning/convert_yolo_labeling_xml.py
+++ material/deep_learning/convert_yolo_labeling_xml.py
@@ -1,9 +1,11 @@
+%cd /content/keras-YOLOv3-model-set
+
 import os
 import glob
 
 CLASS_NAMES = [ "cat", "dog" ] 
 # TAGET_FOLDER_NAME = "labels"
-TAGET_FOLDER_NAME = "dogs-cats"
+TAGET_FOLDER_NAME = "dogs_cats_yolo_labeled"
 
 
 label_file_names = []
@@ -24,27 +26,21 @@ for file_name in label_file_names:
     tree = ET.parse(in_file)
     root = tree.getroot()
 
-    # a_record = [ file_name ]
-    a_record = [ file_name.replace("xml", "jpg") ]  # FIXED =================================
+    a_record = [ file_name.replace("xml", "jpg") ]
 
     for obj in root.iter('object'):
         difficult = obj.find('difficult').text
         cls = obj.find('name').text
         if cls not in CLASS_NAMES or int(difficult)==1:
             continue
-        cls_id = CLASS_NAMES .index(cls)
+        cls_id = str(CLASS_NAMES.index(cls))
         xmlbox = obj.find('bndbox')
-        xmin = int(xmlbox.find('xmin').text)
-        ymin = int(xmlbox.find('ymin').text)
-        # xmax = int(xmlbox.find('xmin').text)    
-        xmax = int(xmlbox.find('xmax').text)  # FIXED =================================
-        ymax = int(xmlbox.find('ymax').text)
+        xmin = xmlbox.find('xmin').text
+        ymin = xmlbox.find('ymin').text
+        xmax = xmlbox.find('xmax').text
+        ymax = xmlbox.find('ymax').text
 
-        a_record.append(xmin)
-        a_record.append(ymin)
-        a_record.append(xmax)
-        a_record.append(ymax)
-        a_record.append(cls_id)
+        a_record.append(",".join([xmin,ymin,xmax,ymax,cls_id]))
 
     all_record.append(a_record)
 
@@ -54,3 +50,5 @@ with open("labels.txt", "w") as f:
     print(a_record)
     f.write(" ".join([str(i) for i in a_record]))
     f.write("\n")
+
+print("labels.txt created.")
